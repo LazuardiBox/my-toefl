a37696eca23b4382a996615af523927787586667
LazuardiBox <satriapulcherzsep@gmail.com>
2026-01-09 05:32:58 +0700

    chore: update orpc route to AppContext

--- src/app/orpc/[[...rest]]/route.ts
+++ src/app/orpc/[[...rest]]/route.ts
@@ -1,3 +1,5 @@
+import type { AppContext } from "@/server/contexts";
+import { db } from "@/server/libraries/drizzle";
 import { rpcHandler } from "@/server/plugins/next";
 
 const prefix = "/orpc";
@@ -5,7 +7,13 @@ const prefix = "/orpc";
 async function handleRequest(request: Request) {
   const { response } = await rpcHandler.handle(request, {
     prefix,
-    context: {},
+    context: {
+      req: request,
+      db,
+      requestId: crypto.randomUUID(),
+      status: "pending",
+      auth: "false",
+    } satisfies AppContext,
   });
 
   return response ?? new Response("Not found", { status: 404 });
