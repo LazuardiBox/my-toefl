57f62eef274075eab80dc728e2e29087a45157ba
LazuardiBox <satriapulcherzsep@gmail.com>
2026-01-09 05:33:52 +0700

    chore: change hook to use protected route

--- src/client/hooks/hello.ts
+++ src/client/hooks/hello.ts
@@ -5,7 +5,7 @@ import { api } from "@/client/utilities/api";
 export function useHelloQuery(enabled = false) {
   return useQuery({
     queryKey: ["hello"],
-    queryFn: () => api.hello({}),
+    queryFn: () => api["hello-protected"]({}),
     enabled,
   });
 }
