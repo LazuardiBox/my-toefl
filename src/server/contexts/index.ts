17241076633ef849887ad128bbf85c40d45b9b86
LazuardiBox <satriapulcherzsep@gmail.com>
2026-01-08 23:10:33 +0700

    chore: remove error, and requestedAt from context

--- src/server/contexts/index.ts
+++ src/server/contexts/index.ts
@@ -5,7 +5,5 @@ import type { Drizzle } from "@/server/libraries/drizzle";
 export type AppContext = {
   db: Drizzle;
   requestId: string;
-  requestedAt: string;
   status: string;
-  error?: string;
 };
