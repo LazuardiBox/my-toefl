ac4c4e5669ead5d36fa358c67e42af29d5c1fa41
LazuardiBox <satriapulcherzsep@gmail.com>
2026-01-09 06:05:03 +0700

    chore: add CORS to hono plugin

--- src/server/plugins/hono.ts
+++ src/server/plugins/hono.ts
@@ -3,6 +3,7 @@
 
 import { onError } from "@orpc/server";
 import { RPCHandler } from "@orpc/server/fetch";
+import { CORSPlugin } from "@orpc/server/plugins";
 import { Hono } from "hono";
 import type { AppContext } from "@/server/contexts";
 import { db } from "@/server/libraries/drizzle";
@@ -11,30 +12,36 @@ import { appRouter } from "@/server/routers";
 const app = new Hono();
 
 const handler = new RPCHandler(appRouter, {
-  interceptors: [
-    onError((error) => {
-      console.error(error);
-    }),
-  ],
+    interceptors: [
+        onError((error) => {
+            console.error(error);
+        }),
+    ],
+    plugins: [
+        new CORSPlugin({
+            origin: (origin) => origin,
+            allowMethods: ["GET", "HEAD", "PUT", "POST", "DELETE", "PATCH"],
+        }),
+    ],
 });
 
 app.use("/orpc/*", async (c, next) => {
-  const { matched, response } = await handler.handle(c.req.raw, {
-    prefix: "/orpc",
-    context: {
-      req: c.req.raw,
-      db,
-      requestId: crypto.randomUUID(),
-      status: "pending",
-      auth: "false",
-    } satisfies AppContext,
-  });
+    const { matched, response } = await handler.handle(c.req.raw, {
+        prefix: "/orpc",
+        context: {
+            req: c.req.raw,
+            db,
+            requestId: crypto.randomUUID(),
+            status: "pending",
+            auth: "false",
+        } satisfies AppContext,
+    });
 
-  if (matched) {
-    return c.newResponse(response.body, response);
-  }
+    if (matched) {
+        return c.newResponse(response.body, response);
+    }
 
-  await next();
+    await next();
 });
 
 export default app;
