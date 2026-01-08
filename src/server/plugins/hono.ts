f2bc053bf85e17a974b577d296ef777515719727
LazuardiBox <satriapulcherzsep@gmail.com>
2026-01-09 06:00:09 +0700

    chore: add hono plugin for migration

--- src/server/plugins/hono.ts
+++ src/server/plugins/hono.ts
@@ -1,40 +1,40 @@
 // @ts-nocheck
 /* eslint-disable */
 
-import { Hono } from 'hono'
-import { RPCHandler } from '@orpc/server/fetch'
-import { onError } from '@orpc/server'
-import { appRouter } from '@/server/routers'
-import { db } from '@/server/libraries/drizzle'
-import type { AppContext } from '@/server/contexts'
+import { onError } from "@orpc/server";
+import { RPCHandler } from "@orpc/server/fetch";
+import { Hono } from "hono";
+import type { AppContext } from "@/server/contexts";
+import { db } from "@/server/libraries/drizzle";
+import { appRouter } from "@/server/routers";
 
-const app = new Hono()
+const app = new Hono();
 
 const handler = new RPCHandler(appRouter, {
-    interceptors: [
-        onError((error) => {
-            console.error(error)
-        }),
-    ],
-})
+  interceptors: [
+    onError((error) => {
+      console.error(error);
+    }),
+  ],
+});
 
-app.use('/orpc/*', async (c, next) => {
-    const { matched, response } = await handler.handle(c.req.raw, {
-        prefix: '/orpc',
-        context: {
-            req: c.req.raw,
-            db,
-            requestId: crypto.randomUUID(),
-            status: "pending",
-            auth: "false",
-        } satisfies AppContext
-    })
+app.use("/orpc/*", async (c, next) => {
+  const { matched, response } = await handler.handle(c.req.raw, {
+    prefix: "/orpc",
+    context: {
+      req: c.req.raw,
+      db,
+      requestId: crypto.randomUUID(),
+      status: "pending",
+      auth: "false",
+    } satisfies AppContext,
+  });
 
-    if (matched) {
-        return c.newResponse(response.body, response)
-    }
+  if (matched) {
+    return c.newResponse(response.body, response);
+  }
 
-    await next()
-})
+  await next();
+});
 
-export default app
+export default app;
