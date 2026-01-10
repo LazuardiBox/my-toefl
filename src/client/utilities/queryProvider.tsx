ff5f7323b55636055175a732bac884a0e49d460d
LazuardiBox <satriapulcherzsep@gmail.com>
2026-01-10 23:35:13 +0700

    chore: disable retry on query provider

--- src/client/utilities/queryProvider.tsx
+++ src/client/utilities/queryProvider.tsx
@@ -4,9 +4,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+          },
+        },
+      })
+  );
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
   );
 }
