4e1ee3cb116064da73fb9bbce6146113d8eb0283
LazuardiBox <satriapulcherzsep@gmail.com>
2026-01-16 01:45:17 +0700

    chore: change oRPC to CSR only

--- src/client/utilities/orpc.ts
+++ src/client/utilities/orpc.ts
@@ -1,25 +1,13 @@
-import { createIsomorphicFn } from '@tanstack/react-start'
-import { getRequestHeaders } from '@tanstack/react-start/server'
 import { createORPCClient } from '@orpc/client'
 import { RPCLink } from '@orpc/client/fetch'
 import type { RouterClient } from '@orpc/server'
 import type { oRPCRouter } from '@/server/routers'
 
-const getORPCClient = createIsomorphicFn()
-  .client((): RouterClient<typeof oRPCRouter> => {
-    const link = new RPCLink({
-      url: `${window.location.origin}/orpc`,
-    })
+const baseURL = import.meta.env.VITE_ORPC_BASE_URL
 
-    return createORPCClient<RouterClient<typeof oRPCRouter>>(link)
-  })
-  .server((): RouterClient<typeof oRPCRouter> => {
-    const link = new RPCLink({
-      url: 'http://localhost:3000/orpc',
-      headers: () => getRequestHeaders(),
-    })
-
-    return createORPCClient<RouterClient<typeof oRPCRouter>>(link)
-  })
-
-export const orpcClient: RouterClient<typeof oRPCRouter> = getORPCClient()
+export const orpcClient: RouterClient<typeof oRPCRouter> =
+  createORPCClient(
+    new RPCLink({
+      url: `${baseURL}/orpc`,
+    }),
+  )
